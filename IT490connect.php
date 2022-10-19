072e07d1c73b86d3779e7ff681b5bd645b7854f1
ccaruso58 <cc58@njit.edu>
2022-10-19 12:41:07 -0400

    host.ini

---
+++ IT490connect.php
@@ -0,0 +1,31 @@
+#!/usr/bin/php
+<?php
+
+$mydb = new mysqli('127.0.0.1','carter','abcde','IT490');
+
+if ($mydb->errno != 0)
+{
+        echo "failed to connect to database: ". $mydb->error . PHP_EOL;
+        exit(0);
+}
+
+echo "successfully connected to database".PHP_EOL;
+
+$query = "select * from users;";
+
+$response = $mydb->query($query);
+if ($mydb->errno != 0)
+{
+        echo "failed to execute query:".PHP_EOL;
+        echo __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
+        exit(0);
+}
+$numrows = mysqli_num_rows($response);
+
+echo "we got $numrows from the query".PHP_EOL;
+
+while ($row = $response->fetch_row())
+{
+	"IT490connect.php" 33L, 631B
+}
+?>
