8c6b43002a8373ae2010505e6a1088046b0b5811
ali-raza-it <ali.raza.esmailzada@gmail.com>
2022-11-09 19:54:20 -0500

    moved stuff

---
+++ Frontend/RabbitMQClient.php
@@ -0,0 +1,38 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+$username = $_POST['LoginInfo'];
+$password = $_POST['Password'];
+
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+if (isset($argv[1]))
+{
+  $msg = $argv[1];
+}
+else
+{
+  $msg = "test message";
+}
+
+$request = array();
+$request['type'] = "login";
+$request['username'] = $username;
+$request['password'] = $password;
+$request['message'] = $msg;
+$response = $client->send_request($request);
+//$response = $client->publish($request);
+
+echo "client received response: ".PHP_EOL;
+print_r($response);
+echo "\n\n";
+if ($response=='true') {
+  header('Location: Frontend/employee.php');
+}
+else {
+  header('Location: Frontend/login.php');
+}
+echo $argv[0]." END".PHP_EOL;
+
