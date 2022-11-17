a66baee8dc153943e2384cfc6dcc4ad56c0095a6
zaidfar <zf5@njit.edu>
2022-11-16 21:22:02 -0500

    added discussion.js

---
+++ Frontend/discussion.js
@@ -0,0 +1,50 @@
+// code referenced from https://github.com/aj-4/5m-forum/blob/main/data.js
+
+var defaultThreads = [
+    {
+        id: 1,
+        title: "Thread 1",
+        author: "Aaron",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "Jack",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "Arthur",
+                date: Date.now(),
+                content: "Hey to you too"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Thread 2",
+        author: "Aaron",
+        date: Date.now(),
+        content: "Thread content 2",
+        comments: [
+            {
+                author: "Jack",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "Arthur",
+                date: Date.now(),
+                content: "Hey to you too"
+            }
+        ]
+    }
+]
+
+var threads = defaultThreads
+if (localStorage && localStorage.getItem('threads')) {
+    threads = JSON.parse(localStorage.getItem('threads'));
+} else {
+    threads = defaultThreads;
+    localStorage.setItem('threads', JSON.stringify(defaultThreads));
+}
