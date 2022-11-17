a02fd1d7df0f3a09a0013a3c8b34cc10b0e48b3b
zaidfar <zf5@njit.edu>
2022-11-16 19:36:44 -0500

    fixed colon in discussion.html

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -29,11 +29,11 @@ echo "client received response: ".PHP_EOL;
 print_r($response);
 echo "\n\n";
 
-//if ($response=='true') {
-//  header('Location: Frontend/employee.php');
-//}
-//else {
-//  header('Location: Frontend/login.php');
-//}
+if ($response=='true') {
+  header('Location: Frontend/landing.php');
+}
+else {
+  header('Location: Frontend/login.php');
+}
 echo $argv[0]." END".PHP_EOL;
 
